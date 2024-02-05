29c7a6e07d3b897f69a38d09a14d7e27d7751bc5
Astanin_1isp-21 <dima.astanin06@mail.ru>
2024-02-05 15:00:14 +0300

    Коммит 2

---
+++ Ostatok.js
@@ -0,0 +1,5 @@
+alert("Остаток от  деления!")
+let a = parseFloat(prompt("Введите число"));
+let b = parseFloat(prompt("Введите число"));
+let c = a % b;
+console.log("Остаток от деления чисел = ", c);
